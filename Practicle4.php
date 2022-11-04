db2229a99ddbfaa253acdbec1d6290216a183490
mightbehacker <108263979+nearlegend@users.noreply.github.com>
2022-11-04 16:17:54 +0530

    Arrys Operations IN php

---
+++ Practicle4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$student = array("Nishit","bariya");
+$cars = array("Volvo", "BMW", "Toyota"); 
+     ;
+echo "My name is:  " . $student[0]  . $student[1]  ;
+?>
+
+</body>
+</html>
+     
